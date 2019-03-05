ff71c0e3a25e08d87055affcbaa0c39d86ae8147
Lenz <Lenz.Furrer@gmail.com>
2019-03-05 16:54:16 +0100

    sub-match postfilter: consider cross-matches as well

--- oger/post/__init__.py
+++ oger/post/__init__.py
@@ -9,5 +9,6 @@ Postprocessing filter functions.
 '''
 
 
+from .submatches import remove_overlaps, remove_sametype_overlaps
 from .submatches import remove_submatches, remove_sametype_submatches
 from .badFP import frequentFP
--- oger/post/submatches.py
+++ oger/post/submatches.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf8
 
-# Author: Lenz Furrer, 2018
+# Author: Lenz Furrer, 2018--2019
 
 
 '''
@@ -10,48 +10,88 @@ Postfilters for removing nested annotations.
 
 
 from collections import defaultdict
+from typing import Sequence, Iterator
 
 from ..doc import document
 
 
-def remove_submatches(content):
+def remove_overlaps(content: document.Exporter) -> None:
     '''
-    Remove annotations that are contained in another one.
+    Remove annotations that partially overlap with another one.
+
+    Of any cluster of overlapping spans, only the longest
+    one(s) are kept.  Ties are not broken.
 
     Ignores entity type.
     '''
-    for sentence in content.get_subelements(document.Sentence):
-        sentence.entities = list(_remove_submatches(sentence.entities))
+    _rm_any_overlaps(content, sametype=False, sub=False)
 
 
-def remove_sametype_submatches(content):
+def remove_sametype_overlaps(content: document.Exporter) -> None:
+    '''
+    Remove same-type annotations that partially overlap with another one.
+
+    Overlapping entities of different type are kept.
+    '''
+    _rm_any_overlaps(content, sametype=True, sub=False)
+
+
+def remove_submatches(content: document.Exporter) -> None:
+    '''
+    Remove annotations that are contained in another one.
+
+    Unlike `remove_overlaps()`, only true subsequences are removed.
+    Consider the following annotation spans:
+        |--------------|     (1)
+        |--------|           (2)
+                    |-----|  (3)
+    This function removes only annotation (2), whereas
+    `remove_overlaps` removes (2) and (3).
+    '''
+    _rm_any_overlaps(content, sametype=False, sub=True)
+
+
+def remove_sametype_submatches(content: document.Exporter) -> None:
     '''
     Remove same-type annotations that are contained in another one.
-
-    Contained of different entity type are kept.
     '''
-    entity_types = defaultdict(list)
+    _rm_any_overlaps(content, sametype=True, sub=True)
+
+
+def _rm_any_overlaps(content: document.Exporter, sametype: bool, sub: bool):
+    if sametype:
+        filter_ = _rm_sametype_overlaps
+    else:
+        filter_ = lambda e, s: list(_rm_overlapping(e, s))
+
     for sentence in content.get_subelements(document.Sentence):
-        # Divide the entities into subsets by entity type.
-        for e in sentence.entities:
-            entity_types[e.type].append(e)
-        # Remove the submatches from each subset.
-        filtered = []
-        for entities in entity_types.values():
-            filtered.extend(_remove_submatches(entities))
-            entities.clear()  # entity_types is reused across sentences
-        filtered.sort(key=document.Entity.sort_key)
-        sentence.entities = filtered
+        sentence.entities = filter_(sentence.entities, sub)
 
 
-def _remove_submatches(entities):
+def _rm_sametype_overlaps(entities, sub):
+    # Divide the entities into subsets by entity type.
+    entity_types = defaultdict(list)
+    for e in entities:
+        entity_types[e.type].append(e)
+    # Remove the submatches from each subset.
+    filtered = []
+    for e in entity_types.values():
+        filtered.extend(_rm_overlapping(e, sub))
+    filtered.sort(key=document.Entity.sort_key)
+    return filtered
+
+
+def _rm_overlapping(entities: Sequence[document.Entity],
+                    sub: bool) -> Iterator[document.Entity]:
     '''
-    Filter out annotations that are contained in another one.
+    Filter out annotations that overlap with others.
     '''
-    # Get the indices of all submatches.
-    removables = set(_submatches(entities))
+    # Get the indices of all removables.
+    filter_ = _submatches if sub else _crossmatches
+    removables = set(filter_(entities))
     # Create a new, filtered list.
     return (e for i, e in enumerate(entities) if i not in removables)
+
 
 def _submatches(entities):
     '''
@@ -70,8 +110,8 @@ def _submatches(entities):
             elif _contains(entity, ref_entity):
                 yield from ref_is
             elif _equals(entity, ref_entity):
-                # If the next entity will contain this one, then the previous needs
-                # to be excluded as well.
+                # If the next entity will contain this one, then the previous
+                # needs to be excluded as well.
                 ref_is.append(i)
                 continue  # keep the previous reference
         # If the current entity was not contained in the reference, then its
@@ -93,3 +133,27 @@ def _equals(a, b):
     Return True if a's and b's offsets are the same.
     '''
     return a.start == b.start and a.end == b.end
+
+
+def _crossmatches(entities):
+    '''
+    Identify partially overlapping entities to be excluded.
+    '''
+    for cluster in _clusters(entities):
+        longest = max(l for _, l in cluster)
+        for i, l in cluster:
+            if l != longest:
+                yield i
+
+def _clusters(entities):
+    cluster = []
+    current_end = 0
+    for i, e in enumerate(entities):
+        if e.start >= current_end:
+            if len(cluster) > 1:
+                yield cluster
+            cluster.clear()
+        current_end = max(current_end, e.end)
+        cluster.append((i, e.end-e.start))
+    if len(cluster) > 1:
+        yield cluster
